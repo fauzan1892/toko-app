b1a2e84113f9a439797327cc1a36ba4928cc78bd
Fauzan Falah <fauzan1892@codekop.com>
2021-12-13 20:56:11 +0700

    Update produk_list.blade.php

--- resources/views/components/frontend/produk_list.blade.php
+++ resources/views/components/frontend/produk_list.blade.php
@@ -1,15 +1,34 @@
-<div class="row">
-    @foreach($produk as $r)
-    <div class="col-sm-3 mb-3">
-        <div class="card-product">
-            <a href="{{ url('produk/'.$r->id) }}" class="text-produk">
-                <img src="{{ url_images('gambar', $r->gambar) }}" class="img-fluid w-100">
-            </a>
-            <div class="clearfix mb-3"></div>
-            <h5 class="text-produk">Rp{{number_format($r->harga_jual)}},-</h5>
-            <a href="{{ url('produk/'.$r->id) }}" class="text-produk">{{ $r->nama_produk }}</a>
-            <div class="clearfix"></div>
+<div class="d-none d-lg-block">
+    <div class="row">
+        @foreach($produk as $r)
+        <div class="col-sm-3 mb-3 d-none d-lg-block">
+            <div class="card-product">
+                <a href="{{ url('produk/'.$r->id) }}" class="text-produk">
+                    <img src="{{ url_images('gambar', $r->gambar) }}" class="img-fluid w-100">
+                </a>
+                <div class="clearfix mb-3"></div>
+                <h5 class="text-produk">Rp{{number_format($r->harga_jual)}},-</h5>
+                <a href="{{ url('produk/'.$r->id) }}" class="text-produk">{{ $r->nama_produk }}</a>
+                <div class="clearfix"></div>
+            </div>  
         </div>  
+        @endforeach
+    </div>
+</div>
+<div class="d-lg-none">
+    <div class="row">
+        @foreach($produk as $r)
+        <div class="col-6 mb-3 d-lg-none">
+            <div class="card-product">
+                <a href="{{ url('produk/'.$r->id) }}" class="text-produk">
+                    <img src="{{ url_images('gambar', $r->gambar) }}" class="img-fluid w-100">
+                </a>
+                <div class="clearfix mb-3"></div>
+                <h5 class="text-produk">Rp{{number_format($r->harga_jual)}},-</h5>
+                <a href="{{ url('produk/'.$r->id) }}" class="text-produk">{{ $r->nama_produk }}</a>
+                <div class="clearfix"></div>
+            </div>  
+        </div>  
+        @endforeach
     </div>  
-    @endforeach
 </div>
