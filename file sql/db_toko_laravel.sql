95a26334d96e3beac47808c9ebb9fc7bacbcabcd
Fauzan Falah <fauzan1892@codekop.com>
2022-01-25 19:11:54 +0700

    update file sql

--- file sql/db_toko_laravel.sql
+++ file sql/db_toko_laravel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 13, 2021 at 02:16 PM
+-- Generation Time: Jan 25, 2022 at 01:11 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 7.4.22
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_toko`
+-- Database: `db_toko_laravel`
 --
 
 -- --------------------------------------------------------
@@ -36,6 +36,33 @@ CREATE TABLE `failed_jobs` (
   `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id` int(11) NOT NULL,
+  `nama_kategori` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id`, `nama_kategori`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Hijab', '2021-12-13 01:50:13', '2021-12-13 02:13:46', NULL),
+(2, 'wad', '2021-12-13 01:51:21', '2021-12-13 02:02:01', '2021-12-13 02:02:01'),
+(3, 'Pashmina', '2021-12-13 02:13:01', '2021-12-13 02:13:38', '2021-12-13 02:13:38'),
+(4, 'Hijab Instan', '2021-12-13 02:13:16', '2021-12-13 02:13:41', '2021-12-13 02:13:41'),
+(5, 'Pakaian Pria', '2021-12-13 02:13:25', NULL, NULL),
+(6, 'Pakaian Wanita', '2021-12-13 02:13:30', NULL, NULL),
+(7, 'Aksesoris', '2021-12-13 02:14:00', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -92,37 +119,10 @@ CREATE TABLE `personal_access_tokens` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_kategori`
+-- Table structure for table `produk`
 --
 
-CREATE TABLE `tbl_kategori` (
-  `id` int(11) NOT NULL,
-  `nama_kategori` varchar(255) DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `deleted_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tbl_kategori`
---
-
-INSERT INTO `tbl_kategori` (`id`, `nama_kategori`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'Hijab', '2021-12-13 01:50:13', '2021-12-13 02:13:46', NULL),
-(2, 'wad', '2021-12-13 01:51:21', '2021-12-13 02:02:01', '2021-12-13 02:02:01'),
-(3, 'Pashmina', '2021-12-13 02:13:01', '2021-12-13 02:13:38', '2021-12-13 02:13:38'),
-(4, 'Hijab Instan', '2021-12-13 02:13:16', '2021-12-13 02:13:41', '2021-12-13 02:13:41'),
-(5, 'Pakaian Pria', '2021-12-13 02:13:25', NULL, NULL),
-(6, 'Pakaian Wanita', '2021-12-13 02:13:30', NULL, NULL),
-(7, 'Aksesoris', '2021-12-13 02:14:00', NULL, NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_produk`
---
-
-CREATE TABLE `tbl_produk` (
+CREATE TABLE `produk` (
   `id` int(11) NOT NULL,
   `id_kategori` int(11) NOT NULL,
   `gambar` varchar(255) DEFAULT NULL,
@@ -135,29 +135,31 @@ CREATE TABLE `tbl_produk` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tbl_produk`
+-- Dumping data for table `produk`
 --
 
-INSERT INTO `tbl_produk` (`id`, `id_kategori`, `gambar`, `nama_produk`, `deskripsi`, `harga_jual`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 1, 'produk_1639388721.jpg', 'HIJAB VOAL SEGI EMPAT - MAISHA SAVANA SERIES', 'Sempurnakan tampilan harianmu dengan hijab segiempat printed berbahan voal yang adem anti gerah, tegak didahi, hijab ini bisa digunakan formal look, daily look dan pemakaian syari ----------------------- Savana Series Senandung Irama tropis padang gurun diluncurkan dalam tema motif scarf hijab. Warna hangat tertuang seiring dengan tumbuhan alam sekitarnya ----------------------- Measurements: 115x115cm Voal Ultrafine', 98000, '2021-12-13 02:45:21', '2021-12-13 03:29:15', NULL),
-(2, 5, 'produk_1639395461.jpg', 'Koko Kurta Agno Big Size', 'Koko Kurta Agno Koko simpel, elegan dan special berbahan katun Toyobo ( polosan ) yang dipadukan dengan motif garis berbahan Yarn Dyed. Desain kerah changi. Terdapat saku pada bagian depan dan samping kanan koko. Bukaan kancing pada bagian dada.  Tangan model 3/4 dengan variasi kancing.  Tersedia 4 pilihan warna Emerald - Latte - Maroon - Grey - Broken White Harga   189.000 S/M/L/XL  199.000 XXL *Available Sarimbit Keluarga Agnian', 200000, '2021-12-13 04:37:41', NULL, NULL),
-(3, 5, 'produk_1639395718.jpg', 'Jersey/Sportwear Oslo Long', 'Jersey/Sportwear Oslo Long dari Gorgeous Indonesia. Koleksi Sport muslim terbaru dengan material yang nyaman dikenakan.', 275000, '2021-12-13 04:41:58', NULL, NULL),
-(4, 6, 'produk_1639395784.jpg', 'New Kaleena Tunic Blue for HIJUP', 'Koleksi terbaru New Kaleena Tunic Blue for HIJUP dari Nadjani', 346500, '2021-12-13 04:43:04', '2021-12-13 04:43:27', NULL),
-(5, 7, 'produk_1639395903.jpg', 'OWIEN WHITE', 'Material : Premium Synthetic Leather K+ Accesories: mute mutiara Insole : Multilayer insole with lalafoam to satisfy your step Outsole : Flexible rubber,slip-resistance & anti crack Flexible Shoe Lace Cannot be Removed Height : 2.5cm Wight : 220 – 270 gram/pcs Proudly Indonesia', 314000, '2021-12-13 04:45:03', NULL, NULL),
-(6, 7, 'produk_1639395986.jpg', 'Kalung Etnik Tenun Batu - Bluesy', 'Ukuran Lingkaran kalung 75 cm, dimensi bandul 9.5 x 6 cm, panjang kalung saat dipakai dari leher bawah 21 cm sampai tali kalung, 31 cm sampai ujung liontin', 198000, '2021-12-13 04:46:26', NULL, NULL),
-(7, 1, 'produk_1639396050.jpg', 'HIJAB VOAL SEGI EMPAT (FREE MASKER) - DYARA SAVANA SERIES', 'Sempurnakan tampilan harianmu dengan hijab segiempat printed berbahan voal yang adem anti gerah, tegak didahi, hijab ini bisa digunakan formal look, daily look dan pemakaian syari Savana Series Senandung Irama tropis padang gurun diluncurkan dalam tema motif scarf hijab. Warna hangat tertuang seiring dengan tumbuhan alam sekitarnya. Measurements: 115x115cm Voal Ultrafine', 116100, '2021-12-13 04:47:30', NULL, NULL),
-(8, 5, 'produk_1639396119.jpeg', 'Ismail Koko Long Sleeves Black Red', 'Ismail Koko Long Sleeves Black Red dari Fatih Indonesia. Koleksi Menswear muslim terbaru dengan material yang nyaman dikenakan.', 399000, '2021-12-13 04:48:39', NULL, NULL);
+INSERT INTO `produk` (`id`, `id_kategori`, `gambar`, `nama_produk`, `deskripsi`, `harga_jual`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 1, 'produk_1643112196.jpg', 'HIJAB VOAL SEGI EMPAT', 'Sempurnakan tampilan harianmu dengan hijab segiempat', 98000, '2021-12-13 02:45:21', '2022-01-25 05:03:16', NULL),
+(2, 5, 'produk_1643112182.jpg', 'Koko Kurta Agno Big Size', 'Koko Kurta Agno Koko simpel, elegan dan special berbahan katun Toyobo', 200000, '2021-12-13 04:37:41', '2022-01-25 05:03:02', NULL),
+(3, 5, 'produk_1643112164.jpg', 'Jersey/Sportwear Oslo Long', 'Jersey/Sportwear Oslo Long', 275000, '2021-12-13 04:41:58', '2022-01-25 05:02:44', NULL),
+(4, 6, 'produk_1643112151.jpg', 'New Kaleena Tunic Blue for HIJUP', 'Koleksi terbaru', 346500, '2021-12-13 04:43:04', '2022-01-25 05:02:31', NULL),
+(5, 7, 'produk_1643112137.jpg', 'OWIEN WHITE', 'Material : Premium Synthetic Leather K+ Accesories', 314000, '2021-12-13 04:45:03', '2022-01-25 05:02:17', NULL),
+(6, 7, 'produk_1643112115.jpg', 'Kalung Etnik Tenun Batu', 'Ukuran Lingkaran kalung 75 cm', 198000, '2021-12-13 04:46:26', '2022-01-25 05:01:55', NULL),
+(7, 1, 'produk_1643112104.jpg', 'HIJAB VOAL SEGI EMPAT', 'Sempurnakan tampilan harianmu dengan hijab segiempat printed berbahan voal yang adem anti gerah, tegak didahi.', 116100, '2021-12-13 04:47:30', '2022-01-25 05:01:44', NULL),
+(8, 5, 'produk_1643112055.jpg', 'Koko Long Sleeves Black Red', 'Koko Long Sleeves Black Red dari Fatih Indonesia.', 399000, '2021-12-13 04:48:39', '2022-01-25 05:01:17', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_users`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `tbl_users` (
+CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(25) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `address` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -166,11 +168,11 @@ CREATE TABLE `tbl_users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `tbl_users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `tbl_users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Fauzan Falah', 'fauzancodekop@gmail.com', NULL, '$2y$10$k.IEYdwVf4ecUVYdOEP2Wugq1NinDjES0kM.sJmuTPIBvcgps0Amy', NULL, '2021-12-13 00:13:32', '2021-12-13 03:45:10');
+INSERT INTO `users` (`id`, `name`, `email`, `phone`, `address`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Fauzan Falah', 'admin@gmail.com', '6281298669897', 'Ujung Harapan Kav. Daruttaqwa 05/14 Kec. Babelan, Kab. Bekasi', NULL, '$2y$10$H83CeR9WQih4Bm.VNCbFt.3pPNI6QrQzxBD6SNtSdKaFpBvfyVqKe', NULL, '2021-12-13 00:13:32', '2022-01-25 04:52:49');
 
 --
 -- Indexes for dumped tables
@@ -182,6 +184,12 @@ INSERT INTO `tbl_users` (`id`, `name`, `email`, `email_verified_at`, `password`,
 ALTER TABLE `failed_jobs`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `migrations`
@@ -204,21 +212,15 @@ ALTER TABLE `personal_access_tokens`
   ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
--- Indexes for table `tbl_kategori`
+-- Indexes for table `produk`
 --
-ALTER TABLE `tbl_kategori`
+ALTER TABLE `produk`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tbl_produk`
+-- Indexes for table `users`
 --
-ALTER TABLE `tbl_produk`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `tbl_users`
---
-ALTER TABLE `tbl_users`
+ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `users_email_unique` (`email`);
 
@@ -233,6 +235,12 @@ ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
@@ -245,21 +253,15 @@ ALTER TABLE `personal_access_tokens`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `tbl_kategori`
+-- AUTO_INCREMENT for table `produk`
 --
-ALTER TABLE `tbl_kategori`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `tbl_produk`
---
-ALTER TABLE `tbl_produk`
+ALTER TABLE `produk`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT for table `tbl_users`
+-- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `tbl_users`
+ALTER TABLE `users`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
